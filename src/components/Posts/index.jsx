76bec979e7e9420b943a18523a2018c6c9c85e9a
Carol Pinheiro <caroline@bossabox.com>
2023-01-16 10:07:22 -0300

    refactor: separated app component into new components

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,14 @@
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts }) => (
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard
+        title={post.title}
+        cover={post.cover}
+        body={post.body}
+        key={post.id}
+      />
+    ))}
+  </div>
+);
